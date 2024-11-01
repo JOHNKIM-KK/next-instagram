ecfe594b401a7d20b97f5ba9ff0c62ade43f433c
김명준 <acspp2017@gmail.com>
2024-11-01 09:29:38 +0900

    feat: 미들웨어 구현

---
+++ src/middleware.ts
@@ -0,0 +1,36 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export const middleware = async (req: NextRequest) => {
+  const token = await getToken({ req });
+
+  if (!token) {
+    if (req.nextUrl.pathname.startsWith("/api")) {
+      return {
+        status: 401,
+        body: "Unauthorized",
+      };
+    }
+    const { pathname, search, origin, basePath } = req.nextUrl;
+    const signInUrl = new URL(`${basePath}/auth/signin`, origin);
+    signInUrl.searchParams.append(
+      "callbackUrl",
+      `${basePath}${pathname}${search}`,
+    );
+    return NextResponse.redirect(signInUrl);
+  }
+
+  return NextResponse.next();
+};
+
+export const conifg = {
+  matcher: [
+    "/new",
+    "/",
+    "/api/bookmarks",
+    "/api/comments",
+    "/api/follow",
+    "/api/me",
+    "/api/posts/:path*",
+  ],
+};
